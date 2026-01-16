0a53fbeb6fbb0faa2564938bd0a77beefca39bcb
aanjeneykodesia-web <aanjeneykodesia@gmail.com>
2026-01-16 19:17:38 +0530

    Delete backend/src/main/java/com/app/entity/repository directory

--- backend/src/main/java/com/app/entity/repository/OrderRepository.java
+++
@@ -1,5 +0,0 @@
-package com.app.repository;
-import org.springframework.data.jpa.repository.JpaRepository;
-import com.app.entity.Orders;
-
-public interface OrderRepository extends JpaRepository<Orders,Long>{}
