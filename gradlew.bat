2d3902eaeabdc0ed32f5a725ac407bf00a8c2618
aanjeneykodesia-web <aanjeneykodesia@gmail.com>
2026-02-14 10:28:43 +0530

    Create gradlew.bat

---
+++ gradlew.bat
@@ -0,0 +1,12 @@
+@echo off
+set DIRNAME=%~dp0
+if "%DIRNAME%" == "" set DIRNAME=.
+set APP_BASE_NAME=%~n0
+set APP_HOME=%DIRNAME%
+
+set DEFAULT_JVM_OPTS=
+
+set JAVA_EXE=java.exe
+if not "%JAVA_HOME%" == "" set JAVA_EXE=%JAVA_HOME%\bin\java.exe
+
+"%JAVA_EXE%" %DEFAULT_JVM_OPTS% -classpath "%APP_HOME%\gradle\wrapper\gradle-wrapper.jar" org.gradle.wrapper.GradleWrapperMain %*
