46eab3fc79cfbc97de65e8e422187c1d413c69ec
aanjeneykodesia-web <aanjeneykodesia@gmail.com>
2026-02-15 14:57:38 +0530

    Add files via upload

---
+++ android-app/app/src/main/LocationHelper.java
@@ -0,0 +1,11 @@
+
+package com.example.supplychain;
+
+import android.content.Context;
+import android.location.Location;
+
+public class LocationHelper {
+    public static Location getLastKnownLocation(Context context) {
+        return null;
+    }
+}
